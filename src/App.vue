b4d96577dc5a1f2e1791194b50788b166f73a3a6
FabrizioCmp <fabriziocampari01@gmail.com>
2022-12-01 14:50:46 +0100

    AppTitle component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+
+  components: { AppTitle },
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="title">
+    <AppTitle></AppTitle>
+    <div class="img_box">
+      <img src="../src/assets/vite.svg" alt="">
+    </div> 
+  </div>
+</template>
+
+<style>
+  .title{
+    text-align: center;
+    margin: 2rem 0;
+  }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,38 @@
+
+<script>
+
+export default {
+
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>title</title>
+    </head>
+
+    <body>
+       <h1>La Prima app con Vite</h1>
+    </body>
+
+    </html>
+</template>
+
+<style>
+    h1{
+        color: brown;
+        text-align: center;
+    }
+</style>
